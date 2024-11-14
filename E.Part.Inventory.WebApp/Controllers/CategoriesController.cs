130a75d12416f4bd36f2b3de0e89eca0405824c5
mk2b79 <samchuk.maks@outlook.com>
2024-11-15 00:10:13 +0200

    Add Categories and Products Controllers with Views

---
+++ E.Part.Inventory.WebApp/Controllers/CategoriesController.cs
@@ -0,0 +1,51 @@
+﻿using E.Part.Inventory.WebApp.DataAccess.Entityes;
+using E.Part.Inventory.WebApp.DataAccess.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E.Part.Inventory.WebApp.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ICrudRepository<Category> _repository;
+
+        public CategoriesController(ICrudRepository<Category> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(_repository.GetAllAsync().Result);
+        }
+        [HttpGet]
+        public IActionResult Create() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            _repository.CreateAsync(category).Wait();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            return View("Create",_repository.GetByIdAsync(id));
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            _repository.UpdateAsync(category).Wait();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id) {
+            _repository.DeleteAsync(id).Wait();
+            return RedirectToAction("Index");
+        }
+    }
+}
