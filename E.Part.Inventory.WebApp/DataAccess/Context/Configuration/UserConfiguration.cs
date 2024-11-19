9007ad33c80ce40fd112eafbe885d65774a89c1c
Samchuk Maxim <samchuk.maks@outlook.com>
2024-11-19 02:01:17 +0200

    Add User

---
+++ E.Part.Inventory.WebApp/DataAccess/Context/Configuration/UserConfiguration.cs
@@ -0,0 +1,33 @@
+using E.Part.Inventory.WebApp.DataAccess.Entityes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace E.Part.Inventory.WebApp.DataAccess.Context.Configuration;
+
+public class UserConfiguration:IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(u => u.Id);
+
+        builder.Property(u => u.Id)
+            .ValueGeneratedOnAdd();
+
+        builder.Property(u => u.UserName)
+            .IsRequired()
+            .IsUnicode()
+            .HasMaxLength(50);
+
+        builder.Property(u => u.Password)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(u => u.Role)
+            .HasConversion(
+                u => u.ToString(),
+                u=>(Role)Enum.Parse(typeof(Role),u)
+                )
+            .IsRequired();
+
+    }
+}
